940e98d8b9fd61ea9266ff0991a031728e1dc9aa
DIPTI <115351486+Dipti-24@users.noreply.github.com>
2025-07-07 12:18:05 +0530

    Update views.py

--- weatherf/views.py
+++ weatherf/views.py
@@ -1,48 +1,38 @@
+import os
 from django.shortcuts import render
 import requests
-import json
 from datetime import datetime
 
-
-# Create your views here.
-
-
-
-    # the index() will handle all the app's logic
 def index(request):
-    # if there are no errors the code inside try will execute
     try:
-    # checking if the method is POST
+        city_weather_update = {}
+
         if request.method == 'POST':
-            API_KEY = '88abad9d9f048098a5515accd4b76833'
-            # getting the city name from the form input   
-            city_name = request.POST.get('city')
-            # the url for current weather, takes city_name and API_KEY   
+            API_KEY = os.getenv('WEATHER_API_KEY')  # safer than hardcoding
+            city_name = request.POST.get('city').strip()
             url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&units=metric'
-            # converting the request response to json   
             response = requests.get(url).json()
-            # getting the current time
+
+            print("API Response:", response)
+
+            if response.get("cod") != 200:
+                return render(request, 'weatherf/404.html', {"error": response.get("message")})
+
             current_time = datetime.now()
-            # formatting the time using directives, it will take this format Day, Month Date Year, Current Time 
             formatted_time = current_time.strftime("%A, %B %d %Y, %H:%M:%S %p")
-            # bundling the weather information in one dictionary
+
             city_weather_update = {
                 'city': city_name,
                 'description': response['weather'][0]['description'],
                 'icon': response['weather'][0]['icon'],
-                'temperature': 'Temperature: ' + str(response['main']['temp']) + ' °C',
+                'temperature': f"Temperature: {response['main']['temp']} °C",
                 'country_code': response['sys']['country'],
-                'wind': 'Wind: ' + str(response['wind']['speed']) + 'km/h',
-                'humidity': 'Humidity: ' + str(response['main']['humidity']) + '%',
+                'wind': f"Wind: {response['wind']['speed']} km/h",
+                'humidity': f"Humidity: {response['main']['humidity']}%",
                 'time': formatted_time
             }
-        # if the request method is GET empty the dictionary
-        else:
-            city_weather_update = {}
-        context = {'city_weather_update': city_weather_update}
-        return render(request, 'weatherf/home.html', context)
-    # if there is an error the 404 page will be rendered 
-    # the except will catch all the errors 
-    except:
-        return render(request, 'weatherf/404.html')
-    
+
+        return render(request, 'weatherf/home.html', {'city_weather_update': city_weather_update})
+
+    except Exception as e:
+        return render(request, 'weatherf/404.html', {"error": str(e)})
